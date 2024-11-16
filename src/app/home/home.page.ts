d3dd59fa67735a1576e30f1e265ee7e935b08314
antoine turello <turello@et.esiea.fr>
2024-11-16 17:45:47 +0100

    Home list front

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {FormsModule} from "@angular/forms";
+import {IonicModule} from "@ionic/angular";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    FormsModule,
+    IonicModule
+  ],
 })
 export class HomePage {
   constructor() {}
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,22 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+import { initializeApp } from "firebase/app";
+import { importProvidersFrom } from "@angular/core";
+import { IonicModule } from "@ionic/angular";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCvVQ1HDg-EI5o2Lx9fXqKEkCyPvuT8QN0",
+  authDomain: "esiea-voiture.firebaseapp.com",
+  projectId: "esiea-voiture",
+  storageBucket: "esiea-voiture.firebasestorage.app",
+  messagingSenderId: "507092553534",
+  appId: "1:507092553534:web:eadfee99f4ab78cb8b266f",
+  measurementId: "G-0FJLF7L0QD"
+};
+
+initializeApp(firebaseConfig);
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
